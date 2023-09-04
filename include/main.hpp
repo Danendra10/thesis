8fdce269cbec9e695dd939def2f5b96b38832d70
Dan Mac <danendraclever24@gmail.com>
2023-09-05 00:09:37 +0700

    whoa i wrote my first hello world, I've done 90% of my thesis

---
+++ include/main.hpp
@@ -0,0 +1,13 @@
+#ifndef MAIN_HPP_
+#define MAIN_HPP_
+#define YAML_CPP_STATIC_DEFINE
+
+// #pragma once
+#include "robot.hpp"
+#include "vec_attr.hpp"
+#include "vec_repl.hpp"
+// #include "yaml-cpp/yaml.h"
+
+#include <iostream>
+
+#endif
